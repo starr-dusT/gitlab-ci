0b4faf33db9375cbd9011c376d528ebe5793050b
Adrian DC <radian.dc@gmail.com>
2020-10-23 11:14:12 +0200

    feat: resolve #79: add 'Bug Reports' and 'Source' links for PyPI

--- setup.py
+++ setup.py
@@ -23,8 +23,10 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://gitlab.com/AdrianDC/gitlabci-local',
     project_urls={
+        'Bug Reports': 'https://gitlab.com/AdrianDC/gitlabci-local/-/issues',
         'Changelog': 'https://gitlab.com/AdrianDC/gitlabci-local/blob/master/CHANGELOG.md',
-        'Documentation': 'https://gitlab.com/AdrianDC/gitlabci-local#gitlabci-local'
+        'Documentation': 'https://gitlab.com/AdrianDC/gitlabci-local#gitlabci-local',
+        'Source': 'https://gitlab.com/AdrianDC/gitlabci-local'
     },
     packages=setuptools.find_packages(exclude=['tests']),
     setup_requires=['setuptools_scm'],
