76e472589a600c7ed5551100dc8ff5b70dafe90b
Adrian DC <radian.dc@gmail.com>
2020-11-26 23:10:05 +0100

    fix: prepare #34: add IS_LINUX and IS_WINDOWS constants

--- gitlabci_local/const.py
+++ gitlabci_local/const.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
+# Libraries
+from sys import platform
+
 # Platform
 class Platform:
+
+    # Constants
+    IS_LINUX = (platform == 'linux' or platform == 'linux2')
+    IS_WINDOWS = (platform == 'win32')
 
     # Paths
     BUILDS_DIR = '/builds'
