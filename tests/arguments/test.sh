075048bbf1990aeb4ffe98d81052419afa984868
Adrian DC <radian.dc@gmail.com>
2020-12-12 12:43:31 +0100

    chore: tests: add 'gitlabci-local -c ./folder/' arguments test

--- tests/arguments/test.sh
+++ tests/arguments/test.sh
@@ -16,6 +16,7 @@ gitlabci-local --update-check
 COLUMNS=40 gitlabci-local --update-check
 CI_LOCAL_UPDATES_OFFLINE=true gitlabci-local --update-check
 gitlabci-local -c unknown/.gitlab-ci.yml && exit 1 || true
+gitlabci-local -c ../../examples/ -d
 gitlabci-local -c ../../examples/.gitlab-ci.yml -d
 gitlabci-local -c ../../examples/.gitlab-ci.yml -d 'Job 1 - 1'
 gitlabci-local -c ../../examples/.gitlab-ci.yml -d -i 'job 1 - 1'
