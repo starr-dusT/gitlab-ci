d6623a313822f1eba1332432f5f4881317b0b497
Adrian DC <radian.dc@gmail.com>
2020-12-14 01:03:50 +0100

    chore: tests: add interactive unit tests with pexpect-executor

--- tests/configurations/test.sh
+++ tests/configurations/test.sh
@@ -9,9 +9,24 @@ cd "${test_path}/"
 set -ex
 
 # Run tests
-# gitlabci-local -p && exit 1 || true
 gitlabci-local -p </dev/null && exit 1 || true
 VARIABLE_8=value8 gitlabci-local -p </dev/null && exit 1 || true
 VARIABLE_8= VARIABLE_11=value11 gitlabci-local -p </dev/null
 gitlabci-local -e VARIABLE_8=value8 -e VARIABLE_11=value11 -p </dev/null
 gitlabci-local -e VARIABLE_8=value8 -e VARIABLE_11=value11 --defaults -p
+pexpect-executor --delay-press 0.2 \
+    --enter \
+    --down --enter \
+    --down --down --enter \
+    --down --down --enter \
+    --press 'input' --enter \
+    --press '_default' --enter \
+    --down --enter \
+    --down --enter \
+    --down --down --enter \
+    --enter \
+    -- gitlabci-local -e VARIABLE_8=value8 -e VARIABLE_11=value11 -p
+pexpect-executor \
+    --enter \
+    --ctrl c \
+    -- gitlabci-local -p && exit -1 || true
---
+++ tests/menus/test.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Access folder
+script_path=$(readlink -f "${0}")
+test_path=$(readlink -f "${script_path%/*}")
+cd "${test_path}/"
+
+# Configure tests
+set -ex
+
+# Run tests
+pexpect-executor --space --enter gitlabci-local
+pexpect-executor --press a --enter gitlabci-local
+pexpect-executor --press a --enter -- gitlabci-local -s 'Job 1'
+pexpect-executor --down --down --space --enter -- gitlabci-local -m
+pexpect-executor --space --enter -- gitlabci-local -p -s
+pexpect-executor --space --enter -- gitlabci-local -p -s menus-1
+pexpect-executor -- gitlabci-local -p -s menus-0
+pexpect-executor --up --up --space --enter -- gitlabci-local -p -m -l
+pexpect-executor --ctrl c -- gitlabci-local -p -m -l
