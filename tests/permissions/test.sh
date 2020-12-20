a4d60ede1e3ae5d245ddaf7225b86c9be599c359
Adrian DC <radian.dc@gmail.com>
2020-12-20 23:13:57 +0100

    chore: test #152: implement permissions tests for temp files

---
+++ tests/permissions/test.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# Access folder
+script_path=$(readlink -f "${0}")
+test_path=$(readlink -f "${script_path%/*}")
+cd "${test_path}/"
+
+# Unsupported host
+USER_ID=$(id -u)
+if [ ! "${USER_ID}" = '0' ]; then
+  echo 'INFO: Test "permissions" ignored as it needs to run in a root container'
+  exit 0
+fi
+
+# Prepare temporary holder
+temp_dir=$(mktemp -d)
+trap "{ rm -rf \"${temp_dir}\"; }" EXIT
+mkdir -p "${temp_dir}/holder"
+
+# Prepare temporary clones
+mkdir -p "${temp_dir}/holder/parent/"
+cp -rf "${test_path}" "${temp_dir}/holder/parent/test"
+cp -rf "${test_path}" "${temp_dir}/holder/root"
+cp -rf "${test_path}" "${temp_dir}/holder/test"
+
+# Prepare temporary user
+id -u test >/dev/null 2>&1 || useradd --create-home test
+
+# Prepare temporary permissions
+chmod 755 "${temp_dir}"
+chmod 755 "${temp_dir}/holder"
+chmod 755 "${temp_dir}/holder/parent"
+chown -R test:test "${temp_dir}/holder/parent"
+chown -R test:test "${temp_dir}/holder/test"
+
+# Configure tests
+set -ex
+
+# Run tests
+sudo -H -u test -E env PYTHONPATH="${PYTHONPATH}" gitlabci-local -p
+sudo -H -u test -E env PYTHONPATH="${PYTHONPATH}" gitlabci-local -c "${temp_dir}/holder/root/" -p && exit 1 || true
+sudo -H -u test -E env PYTHONPATH="${PYTHONPATH}" gitlabci-local -c "${temp_dir}/holder/parent/test/" -p
+sudo -H -u test -E env PYTHONPATH="${PYTHONPATH}" gitlabci-local -c "${temp_dir}/holder/test/" -p
+find ../ -name '.tmp.entrypoint.*' -print -exec false {} +
+find "${temp_dir}/holder/parent/test/" -name '.tmp.entrypoint.*' -print -exec false {} +
+find "${temp_dir}/holder/test/" -name '.tmp.entrypoint.*' -print -exec false {} +
