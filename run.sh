2cdf8055c2dde699781e2de3cad7bedeb644767f
Adrian DC <radian.dc@gmail.com>
2020-12-21 16:22:42 +0100

    chore: run: adapt 'run.sh' to missing sudo and wine support

--- run.sh
+++ run.sh
@@ -18,9 +18,14 @@ echo ''
 # Execute job scripts
 sed -n "/^'${JOB}.*':$/,/^$/{ /^  script:$/{ :a; n; /    - /{ s/^ *- //p; ba; } } }" ./.gitlab-ci.yml |
   while read -r line; do
-    if [ "${OSTYPE}" = 'msys' ]; then
-      line=${line//sudo /}
-      line=${line//python3 /python }
+    if ! type sudo >/dev/null 2>&1 || [ "${OSTYPE}" = 'msys' ]; then
+      line=$(echo "${line}" | sed 's/sudo //g')
+    fi
+    if ! type python3 >/dev/null 2>&1 || [ "${OSTYPE}" = 'msys' ]; then
+      line=$(echo "${line}" | sed 's/python3 /python /g')
+    fi
+    if ! type python >/dev/null 2>&1 && type wine >/dev/null 2>&1; then
+      line=$(echo "${line}" | sed 's/python /wine python /g')
     fi
     echo "+ ${line}"
     sh -c "${line}"
