3c5d2d5c1eb5f7e3c7c7c58de9eed756a69609a8
Adrian DC <radian.dc@gmail.com>
2020-12-21 23:52:36 +0100

    chore: test: finish parser coverage of .env environment override

--- tests/environment/test.sh
+++ tests/environment/test.sh
@@ -13,5 +13,6 @@ gitlabci-local -p
 gitlabci-local -e USER -e VALUE_1=set1 -e VALUE_2=set2 -e VALUE_3=set3 -p
 gitlabci-local -e FOLDER="$(readlink -f "${PWD}")" -v ./ 'Job 2'
 gitlabci-local -e environment.env -p
+VALUE_5=value_5 gitlabci-local -e environment.env -p
 gitlabci-local -v ./:/opt 'Job 2'
 gitlabci-local -v ./:/opt -w /opt/ 'Job 2'
