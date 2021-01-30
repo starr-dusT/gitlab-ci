2e92347bed5309579e38ba43edc7f06b46643e33
Adrian DC <radian.dc@gmail.com>
2021-01-30 13:23:19 +0100

    test: test #156: resolve 'project' variables being now handled

--- tests/project/test.sh
+++ tests/project/test.sh
@@ -15,5 +15,6 @@ gitlabci-local 'Job 2' && exit 1 || true
 gitlabci-local -e CI_VARIABLE_DIR=. 'Job 2'
 CI_VARIABLE_DIR=. gitlabci-local 'Job 2' && exit 1 || true
 CI_VARIABLE_DIR=. gitlabci-local -e CI_VARIABLE_DIR 'Job 2'
-gitlabci-local 'Job 3' && exit 1 || true
+gitlabci-local 'Job 3'
+gitlabci-local 'Job 4' && exit 1 || true
 gitlabci-local -e CI_CONSTANT_PRE_DIR=. -e CI_CONSTANT_POST_DIR=. 'Job 3'
