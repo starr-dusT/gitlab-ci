10c23c884010ca388c33c0624f2fe35de599f703
Adrian DC <radian.dc@gmail.com>
2021-02-12 21:46:17 +0100

    tests: test #160: run sockets tests only Docker supported hosts

--- tests/sockets/test.sh
+++ tests/sockets/test.sh
@@ -8,6 +8,15 @@ cd "${test_path}/"
 # Configure tests
 set -ex
 
+# Detect engines
+engine_docker=$(type docker >/dev/null 2>&1 && docker --help | grep -iq 'Docker version' && echo 'true' || echo "${DOCKER_HOST}")
+
+# Non-Docker hosts
+if [ -z "${engine_docker}" ]; then
+  echo 'WARNING: The sockets tests are ignored on non-Docker hosts'
+  exit 0
+fi
+
 # Run tests
 timeout 5 gitlabci-local -c ./.gitlab-ci.incomplete.yml -p && exit 1 || true
 timeout 5 gitlabci-local -c ./.gitlab-ci.incomplete.yml -p --sockets
