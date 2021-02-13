a993aa4fa3d2aec323491077a01f6e146c7cf65c
Adrian DC <radian.dc@gmail.com>
2021-02-13 15:32:03 +0100

    tests: test #167: add a coverage test for engine network modes

--- tests/engines/test.sh
+++ tests/engines/test.sh
@@ -10,6 +10,7 @@ set -ex
 
 # Run tests
 gitlabci-local -p
+gitlabci-local -n none -p
 DOCKER_HOST=tcp://0.0.0.0:9999 gitlabci-local -E d -p && exit 1 || true
 gitlabci-local -E auto -p
 CI_LOCAL_ENGINE=docker,podman,auto gitlabci-local -p
